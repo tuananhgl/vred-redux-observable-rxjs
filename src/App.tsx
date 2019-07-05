679049e64c13601aa6fbc55f058fb039b41e0a6c
Tuan Anh <tuananh.smart17@gmail.com>
2019-07-05 10:17:42 +0700

    Init code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { Switch, Route } from 'react-router-dom';
+import Employees from './containers/employees/employees';
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className="content-wrapper">
+                <Switch>
+                    <Route path={'/'} component={ Employees } exact></Route>
+                </Switch>
+            </div>
+        </div>
+    );
 }
 
 export default App;
